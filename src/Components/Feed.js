a978de5aec34dab9405c857aabc998ca50bddcc5
Matthew Nguyen <matthewnguyen@Matthew-Nguyens-MacBook-Air.local>
2022-10-21 16:50:51 -0700

    final

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -6,15 +6,15 @@ import Block from './Block';
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
+    const [post, setPosts] = useState([]);
 
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
-
+    const posts = post.map((post) => {
+        return (<Block color={post.color} />);
+    });
     return (
         <div>
-            <Menu></Menu>
-
-            {/* Below is where all of your Blocks should render! */}
+            <Menu handleClick={(color) => setPosts([{color: color}].concat(post))}></Menu>
             {posts}
         </div>
     );
